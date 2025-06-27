f605ed35267ef1715d16349139e40ff54058393d
Kavita-LachmanDas <kavitaluhana11@gmail.com>
2025-06-27 09:44:35 -0700

    create api using mongoose,express (backend)

--- db.js
+++ db.js
@@ -1,4 +1,13 @@
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 
-mongoose.connect(`mongodb+srv://bji43268:bji12@cluster0.vw35d1d.mongodb.net/e-commerce?retryWrites=true&w=majority&appName=Cluster0`)
-.then(()=>{console.log('db is connected')}).catch((err)=>{console.log('db is not connected',err)})
+// mongoose.connect(`mongodb+srv://bji43268:bji12@cluster0.vw35d1d.mongodb.net/e-commerce?retryWrites=true&w=majority&appName=Cluster0`)
+// .then(()=>{console.log('db is connected')}).catch((err)=>{console.log('db is not connected',err)})
+
+const mongoose = require('mongoose');
+require('dotenv').config(); 
+
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error(' MongoDB error:', err));
